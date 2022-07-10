b9fed49c7025db76f839be67de2429cd88f3135c
Gspr-bit <gasparhs400@gmail.com>
2022-07-09 19:07:06 -0500

    v0.1 - Two players

--- main.cpp
+++ main.cpp
@@ -15,14 +15,13 @@ const u16 WIDTH = 7;
 const u16 HEIGHT = 6;
 
 u16 display[HEIGHT][WIDTH] = {0};
-u16 display_map[HEIGHT][WIDTH] = {0};
 u16 height[WIDTH] = {0};
 
 void print_display ()
 {
-  for (int i = HEIGHT - 1; i >= 0; i--)
+  for (i16 i = HEIGHT - 1; i >= 0; i--)
     {
-      for (int j = 0; j < WIDTH; j++)
+      for (u16 j = 0; j < WIDTH; j++)
         {
           cout << ICONS[display[i][j]];
         }
@@ -43,31 +42,28 @@ bool is_inside_display (i16 x, i16 y)
 u16 update_map (u16 column)
 {
   // directions down-right,  down-left, down, right
-  i16 dx[4] = {1, -1, 0, 1};
   i16 dy[4] = {-1, -1, -1, 0};
+  i16 dx[4] = {1, -1, 0, 1};
 
   u16 largest = 1;
 
-  cout << "UPDATE MAP\n";
-  print_display ();
-
   // for each direction
-  for (int i = 0; i < 4; i++)
+  for (i16 i = 0; i < 4; i++)
     {
       u16 size = 1;
       i16 x = (i16) column;
-      i16 y = height[column] - 1;
+      i16 y = i16(height[column] - 1);
       const i16 x2 = x, y2 = y;
 
       // first one direction
-      while (is_inside_display(x+=dx[i], y+=dy[i]) and display[x][y] == display[x2][y2])
+      while (is_inside_display (x += dx[i], y += dy[i]) and display[y][x] == display[y2][x2])
         size += 1;
 
       x = x2;
       y = y2;
 
       // then the other
-      while (is_inside_display(x-=dx[i], y-=dy[i]) and display[x][y] == display[x2][y2])
+      while (is_inside_display (x -= dx[i], y -= dy[i]) and display[y][x] == display[y2][x2])
         size += 1;
 
       // update the largest size
@@ -110,7 +106,7 @@ int main ()
   bool turn = true; // Player 1 turn
 
   // There can only be 42 moves in the game
-  for (int i = 0; i < 42; i++)
+  for (u16 i = 0; i < 42; i++)
     {
       print_display ();
 
@@ -121,12 +117,14 @@ int main ()
       if (update_map (column) >= 4)
         {
           cout << "Player " << (turn ? 1 : 2) << " wins!\n";
+          print_display ();
           return 0;
         }
 
       turn = !turn;
     }
 
+  print_display ();
   cout << "It's a tie\n";
 
   return 0;
